3d2ee73b26b41411027a28bb79325f806b607916
wimba juventio chandra <wimbajuventio@yahoo.com>
2019-05-15 12:23:09 +0700

    initial commit

---
+++ LearnerOOP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LearnerOOP
+//
+//  Created by Wimba Juventio Chandra on 15/05/19.
+//  Copyright © 2019 Wimba Juventio Chandra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
